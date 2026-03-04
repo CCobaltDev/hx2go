c53d410b1f27b46acc2a4032b10343da7fab292b
mikaib <mikaib@duck.com>
2026-03-04 10:37:02 +0100

    initial test case

--- testbed/Test.hx
+++ testbed/Test.hx
@@ -10,33 +10,13 @@ class Wee {
 
 class Waa extends Wee {}
 
+@:analyzer(ignore)
 class Test {
 
     static function main() {
-        var cls: Dynamic = new Waa(12.34);
-        var obj: Dynamic = {
-            x: 5,
-            y: {
-                foo: 10,
-                bar: 20
-            }
-        };
-
-        Sys.println("read");
-        Sys.println("cls.waa: " + cls.waa);
-        Sys.println("obj.x: " + obj.x);
-        Sys.println("obj.y: " + obj.y);
-        Sys.println("obj.y.foo: " + obj.y.foo);
-        Sys.println("obj.y.bar: " + obj.y.bar);
-         Sys.println(obj.y.foo + obj.y.bar + cls.waa);
-
-        Sys.println("write");
-        Sys.println("before: " + obj.y.foo + " and " + cls.waa);
-        obj.y.foo = 20;
-        obj.y.foo *= 2;
-        cls.waa = 56.78;
-        cls.waa -= 0.01;
-        Sys.println("after: " + obj.y.foo + " and " + cls.waa);
+        var arr: Dynamic = [1, 2, 3];
+        Sys.println(arr);
+        Sys.println(arr[0]);
     }
 
 }
